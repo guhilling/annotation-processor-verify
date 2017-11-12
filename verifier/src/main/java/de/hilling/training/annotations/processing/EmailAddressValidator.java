6c229d1b31b644e65ca628e9400bd84d3211f07a
Gunnar Hilling <gunnar@hilling.de>
2017-11-12 10:20:37 +0100

    - refactor - add demo project

--- verifier/src/main/java/de/hilling/training/annotations/processing/EmailAddressValidator.java
+++ verifier/src/main/java/de/hilling/training/annotations/processing/EmailAddressValidator.java
@@ -20,29 +20,30 @@ import javax.tools.Diagnostic;
 @SupportedAnnotationTypes("de.hilling.training.annotations.processing.EMailAddress")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class EmailAddressValidator extends AbstractProcessor {
-// end::declaration[]
+    // end::declaration[]
 
-    public static final String DEFAULT_ERROR_MESSAGE = EMailAddress.class.getSimpleName() + " annotated field must be" +
-                                                       " a String";
+    static final String DEFAULT_ERROR_MESSAGE = EMailAddress.class
+                                                       .getSimpleName() + " annotated field must be" + " a String";
 
     private Messager messager;
+    private Types    types;
+    private Elements elements;
 
     @Override
-    public void init(ProcessingEnvironment env) {
-        messager = env.getMessager();
-        processingEnv = env;
+    public void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        messager = processingEnv.getMessager();
+        types = processingEnv.getTypeUtils();
+        elements = processingEnv.getElementUtils();
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        final Types types = processingEnv.getTypeUtils();
-        final Elements elements = processingEnv.getElementUtils();
-
         final Set<? extends Element> annotatedFields = roundEnv.getElementsAnnotatedWith(EMailAddress.class);
         for (Element mailField : annotatedFields) {
             final TypeMirror annotatedType = mailField.asType();
             final TypeMirror expectedType = elements.getTypeElement(String.class.getCanonicalName()).asType();
-            if(!types.isSameType(annotatedType, expectedType)) {
+            if (!types.isSameType(annotatedType, expectedType)) {
                 messager.printMessage(Diagnostic.Kind.ERROR, DEFAULT_ERROR_MESSAGE, mailField);
             }
         }
