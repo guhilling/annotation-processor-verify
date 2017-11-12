fb0ad927e89d448ea749d2089a81e186cebb617f
Gunnar Hilling <gunnar@hilling.de>
2017-11-12 15:09:40 +0100

    - simplify

--- verifier/src/main/java/de/hilling/training/annotations/processing/EmailAddressValidator.java
+++ verifier/src/main/java/de/hilling/training/annotations/processing/EmailAddressValidator.java
@@ -1,13 +1,12 @@
 package de.hilling.training.annotations.processing;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
@@ -17,17 +16,29 @@ import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 
 // tag::declaration[]
-@SupportedAnnotationTypes("de.hilling.training.annotations.processing.EMailAddress")
-@SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class EmailAddressValidator extends AbstractProcessor {
     // end::declaration[]
 
     static final String DEFAULT_ERROR_MESSAGE = EMailAddress.class
-                                                       .getSimpleName() + " annotated field must be" + " a String";
+                                                .getSimpleName() + " annotated field must be" + " a String";
 
     private Messager messager;
     private Types    types;
     private Elements elements;
+
+    // tag::registration[]
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> result = new HashSet<>();
+        result.add(EMailAddress.class.getCanonicalName());
+        return result;
+    }
+    // end::registration[]
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
 
     @Override
     public void init(ProcessingEnvironment processingEnv) {
@@ -37,16 +48,22 @@ public class EmailAddressValidator extends AbstractProcessor {
         elements = processingEnv.getElementUtils();
     }
 
+    // tag::implementation[]
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         final Set<? extends Element> annotatedFields = roundEnv.getElementsAnnotatedWith(EMailAddress.class);
         for (Element mailField : annotatedFields) {
-            final TypeMirror annotatedType = mailField.asType();
-            final TypeMirror expectedType = elements.getTypeElement(String.class.getCanonicalName()).asType();
-            if (!types.isSameType(annotatedType, expectedType)) {
-                messager.printMessage(Diagnostic.Kind.ERROR, DEFAULT_ERROR_MESSAGE, mailField);
-            }
+            verifyElement(mailField);
         }
         return false;
+    }
+    // end::implementation[]
+
+    private void verifyElement(Element mailField) {
+        final TypeMirror annotatedType = mailField.asType();
+        final TypeMirror expectedType = elements.getTypeElement(String.class.getCanonicalName()).asType();
+        if (!types.isSameType(annotatedType, expectedType)) {
+            messager.printMessage(Diagnostic.Kind.ERROR, DEFAULT_ERROR_MESSAGE, mailField);
+        }
     }
 }
