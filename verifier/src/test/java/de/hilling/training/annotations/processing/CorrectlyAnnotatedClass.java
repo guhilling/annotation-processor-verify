78e94a84e647bf64a66bce091e2b95659c90a216
Gunnar Hilling <gunnar@hilling.de>
2017-11-14 09:50:30 +0100

    example.

--- verifier/src/test/java/de/hilling/training/annotations/processing/CorrectlyAnnotatedClass.java
+++ verifier/src/test/java/de/hilling/training/annotations/processing/CorrectlyAnnotatedClass.java
@@ -1,5 +1,8 @@
 package de.hilling.training.annotations.processing;
 
+/**
+ * Beispiel für Klasse mit korrekter e-mail-Adresse.
+ */
 public class CorrectlyAnnotatedClass {
     @EMailAddress
     private String email;
